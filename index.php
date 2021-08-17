c02064ba8b214f12c07a24e91521ada3468da4f4
RubinhoMoraeslitz <85366243+RubinhoMoraeslitz@users.noreply.github.com>
2021-08-17 16:35:50 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,29 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//Carrega um usuario
+//$root = new Usuario();
+//$root->loadbyId(1);
+//echo $root;
 
-$root->loadbyId(1);
 
-echo $root;
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+//carrega uma lista de ususrios buscando pleo lgin
+
+//$search = Usuario::search("oo");
+
+//echo json_encode($search);
+
+//carrega um usuario usando login e senha
+
+$usuario = new Usuario();
+$usuario->login("moraeslitz","uyhbdwe");
+
+echo $usuario;
+
 
 ?>
